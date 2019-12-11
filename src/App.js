f49b65cfc04324c097489c157148964f65cdbfeb
kaiapalm <palmkaia@gmail.com>
2019-12-11 13:52:52 +0200

    converted to functional component, added axios

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      weather: null,
-      city: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const App = () => {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
 
-  handleChange(event) {
-    this.setState({ city: event.target.value });
-  }
+  const handleChange = event => {
+    setCity(event.target.value);
+  };
 
-  handleSubmit(event) {
-    fetch(
-      "http://api.openweathermap.org/data/2.5/weather?APPID=b5b5be990915bf18f851f86d73398368&units=metric&q=" +
-        this.state.city
-    )
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ weather: data });
+  const handleSubmit = event => {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?APPID=b5b5be990915bf18f851f86d73398368&units=metric&q=" +
+          city
+      )
+      .then(response => {
+        setWeather(response.data);
       })
-      .catch(console.log);
-  }
+      .catch(error => {
+        console.error(error);
+        alert(error);
+      });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <label>
-            City:
-            <input
-              type="text"
-              value={this.state.city}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button onClick={this.handleSubmit}>Fetch</button>
-          {this.state.weather && (
-            <div>
-              <h2>{this.state.weather.main.temp}</h2>
-              <h2>{this.state.weather.main.humidity}</h2>
-              <h2>{this.state.weather.wind.speed}</h2>
-            </div>
-          )}
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <label htmlFor="city">City:</label>
+        <input
+          className="Input"
+          type="text"
+          id="city"
+          value={city}
+          onChange={handleChange}
+        />
+        <button className="Button" onClick={handleSubmit}>
+          Fetch
+        </button>
+        {weather && (
+          <div>
+            <div className="Text">Temperature: {weather.main.temp} C</div>
+            <div className="Text">Humidity: {weather.main.humidity} %</div>
+            <div className="Text">Wind speed: {weather.wind.speed} m/s</div>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+};
 
 export default App;
